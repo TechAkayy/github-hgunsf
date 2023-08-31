0d73c819ca5c747ddb8eb145d24528cfe174ac0a
Akayy <techakayy@gmail.com>
2023-08-31 14:45:27 +1000

    Added site config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/alpine',
   modules: ['nuxt-og-image'],
 
+  site: {
+    url: 'https://example.com',
+    name: 'My Site',
+    // ...etc
+  },
+
   ogImage: {
     defaults: {
       component: 'OgImage',
     },
   },
-});
+})
